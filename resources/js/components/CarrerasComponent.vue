6b72016eb77f2e3622e30d9458912c91eb9aa6bf
bryan956 <131468909+bryan956@users.noreply.github.com>
2023-11-20 06:28:41 +0100

    menu y rutas

---
+++ resources/js/components/CarrerasComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="carrera in carreras" :key="carrera.id">
+      {{ carrera.nombre }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default {
+setup() {
+  const carreras = ref([]);
+
+  onMounted(async () => {
+    try {
+      const response = await axios.get('/api/carreras');
+      carreras.value = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  return {
+    carreras
+  };
+},
+};
+</script>
